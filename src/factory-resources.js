452834babe835891817b85864dc1cc0e00c70d26
Milo Casagrande <milo@foundries.io>
2020-11-04 16:48:25 +0100

    update: factory resources
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/factory-resources.js
+++ src/factory-resources.js
@@ -6,11 +6,12 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 */
 
-import JobServ from './jobserv';
+import JobServ from './jobserv.js';
 
 export class FactoryResources extends JobServ {
-  constructor(uri, cache) {
-    super(uri, cache);
+  constructor(address) {
+    super(address);
+    this.basePath = '/loops/';
   }
 }
 
