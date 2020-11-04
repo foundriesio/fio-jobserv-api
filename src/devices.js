293e427621c30defd5cf2a7f34b720ff89b07015
Milo Casagrande <milo@foundries.io>
2020-11-04 16:45:19 +0100

    update: devices resource
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/devices.js
+++ src/devices.js
@@ -6,53 +6,199 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 */
 
-import JobServ from './jobserv';
+import createResponse from './response.js';
+import JobServ from './jobserv.js';
 
 class Devices extends JobServ {
-  constructor(uri, cache) {
-    super(uri, cache);
+  constructor(address) {
+    super(address);
+    this.basePath = '/ota/devices/';
   }
 }
 
 /**
  * Find a device by its name.
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
  */
-Devices.prototype.findByName = async function ({ user, deviceName, query }) {
-  return this.findById({ user, deviceName, query });
+Devices.prototype.findByName = async function ({ device, query, options }) {
+  return this.find({ path: `${device}/`, query, options });
+};
+
+/**
+ * Remove a device.
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
+ */
+Devices.prototype.remove = async function ({ device, query, options }) {
+  return createResponse(this.delete({ path: `${device}/`, query, options }));
+};
+
+/**
+ * Update a device name.
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {String|Buffer} data.body - The data to send.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
+ */
+Devices.prototype.update = async function ({ device, body, query, options }) {
+  return createResponse(
+    this.patch({
+      path: `${device}/updates/`,
+      body,
+      query,
+      options,
+    })
+  );
 };
 
 /**
  * Retrieve all updates of a device.
- * @param {Object} user
- * @param {String} deviceName
- * @param {Object} query
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
  */
-Devices.prototype.updates = async function ({ user, deviceName, query }) {
+Devices.prototype.getUpdates = async function ({ device, query, options }) {
   return this.find({
-    user: user,
-    path: `${deviceName}/updates/`,
-    query: query,
+    path: `${device}/updates/`,
+    query,
+    options,
   });
 };
 
 /**
- * Retrieve a single device update.
- * @param {Object} user
- * @param {String} deviceName
- * @param {String} updateId
- * @param {Object} query
+ * Retrieve events that took place during the update.
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {String} data.correlationId - The update/correlation id.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
  */
-Devices.prototype.findUpdateById = async function ({
-  user,
-  deviceName,
-  updateId,
+Devices.prototype.getUpdateEvents = async function ({
+  device,
+  correlationId,
   query,
+  options,
 }) {
   return this.find({
-    user: user,
-    query: query,
-    path: `${deviceName}/updates/${updateId}/`,
+    path: `${device}/updates/${correlationId}/`,
+    query,
+    options,
   });
+};
+
+/**
+ * Retrieve the list of config settings defined for a device.
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {String} data.update - The update name/id.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
+ */
+Devices.prototype.getUpdateById = async function ({
+  device,
+  update,
+  query,
+  options,
+}) {
+  return this.find({
+    path: `${device}/updates/${update}/`,
+    query,
+    options,
+  });
+};
+
+/**
+ * Retrieve the list of config settings defined for a device.
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
+ */
+Devices.prototype.createConfig = async function ({ device, query, options }) {
+  return this.find({
+    path: `${device}/config/`,
+    query,
+    options,
+  });
+};
+
+/**
+ * Retrieve the list of config settings defined for a device.
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
+ */
+Devices.prototype.getConfig = async function ({ device, query, options }) {
+  return this.find({
+    path: `${device}/config/`,
+    query,
+    options,
+  });
+};
+
+/**
+ * Update the device configuration with new content.
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {String|Buffer} data.body - The data to send.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
+ */
+Devices.prototype.updateConfig = async function ({
+  device,
+  body,
+  query,
+  options,
+}) {
+  return createResponse(
+    this.patch({
+      path: `${device}/config/`,
+      body,
+      query,
+      options,
+    })
+  );
+};
+
+/**
+ * Remove the config file from the configuration.
+ * @param {Object} data
+ * @param {String} data.device - The name of the device.
+ * @param {String} data.config - The name of the config to remove.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
+ */
+Devices.prototype.removeConfig = async function ({
+  device,
+  config,
+  query,
+  options,
+}) {
+  return createResponse(
+    this.delete({
+      path: `${device}/config/${config}/`,
+      query,
+      options,
+    })
+  );
 };
 
 export default Devices;
