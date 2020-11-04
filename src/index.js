e66c5cb00674f06593b99afc91a746d828fd4769
Milo Casagrande <milo@foundries.io>
2020-11-04 16:50:21 +0100

    update: jobserv and default resources
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/index.js
+++ src/index.js
@@ -6,26 +6,49 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 */
 
-import createresponse from './response';
-import devices from './devices';
-import factories from './factories';
-import factoryresources from './factory-resources';
-import jobserv from './jobserv';
-import legacydevicegroups from './legacy-device-groups';
-import legacydevices from './legacy-devices';
-import projects from './projects';
-import updates from './updates';
-import remote from './remote';
-
-export const createResponse = createresponse;
-export const Devices = devices;
-export const Factories = factories;
-export const FactoryResources = factoryresources;
-export const JobServ = jobserv;
-export const LegacyDeviceGroups = legacydevicegroups;
-export const LegacyDevices = legacydevices;
-export const Projects = projects;
-export const Remote = remote;
-export const Updates = updates;
+import Devices from './devices.js';
+import Factories from './factories.js';
+import FactoryResources from './factory-resources.js';
+import JobServ from './jobserv.js';
+import LegacyDeviceGroups from './legacy-device-groups.js';
+import LegacyDevices from './legacy-devices.js';
+import Projects from './projects.js';
+import Updates from './updates.js';
 
 export default JobServ;
+
+/**
+ * Generate the base API access resources.
+ * Available resources are:
+ *   Devices
+ *   Factories
+ *   LegacyDeviceGroups
+ *   LegacyDevices
+ *   Projects
+ *   Updates
+ * @param {String} address - The address of the resource server.
+ * @returns {Object}
+ */
+export const generateFioApiResources = (address) => {
+  return {
+    Devices: new Devices(address),
+    Factories: new Factories(address),
+    LegacyDeviceGroups: new LegacyDeviceGroups(address),
+    LegacyDevices: new LegacyDevices(address),
+    Projects: new Projects(address),
+    Updates: new Updates(address),
+  };
+};
+
+/**
+ * Generate the internal API access resources.
+ * Available resources are:
+ *   FactoryResources
+ * @param {String} address - The address of the resource server.
+ * @returns {Object}
+ */
+export const generateFioInternalApiResources = (address) => {
+  return {
+    FactoryResources: new FactoryResources(address),
+  };
+};
--- src/jobserv.js
+++ src/jobserv.js
@@ -6,137 +6,91 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 */
 
-import createResponse from './response';
-import getSecrets from './secrets';
-import Remote from './remote';
+import createResponse from './response.js';
+import Remote from './remote.js';
 
 export class JobServ extends Remote {
-  constructor(uri, cache) {
-    super(uri);
-    this.follows = true;
-    this.secrets = getSecrets(cache);
+  constructor(address) {
+    super(address);
   }
 }
 
 /**
- * Serialize the data to be sent.
- * It uses JSON.stringify() to serialize the data.
- *
- * @param {Object} data The data to be sent.
- * @returns {String} Serialized data as a string.
+ * Retrieve all data at the specified path.
+ * @param {Object} data
+ * @param {String} [data.path] - The path of the request.
+ * @param {Object} [data.query] - The query parameters.
+ * @param {Object} [data.options] - Optional request configurations.
+ * @returns {Promise<Object>}
  */
-JobServ.prototype.serialize = function (data) {
-  if (data) {
-    return JSON.stringify(data);
-  }
+JobServ.prototype.list = async function ({ path, query, options }) {
+  return createResponse(this.get({ path, query, options }));
 };
 
 /**
- * Prepare the request options.
- *
- * @param {Object} user The user performing the request.
- * @param {Boolean} hasData If the request has data to be sent, default to false.
- * @returns {Promise} Resolves to an Object.
+ * Get all data at the specified path.
+ * @param {Object} data
+ * @param {String} [data.path] - The path of the request.
+ * @param {Object} [data.query] - The query parameters.
+ * @param {Object} [data.options] - Optional request configurations.
+ * @returns {Promise<Object>}
  */
-JobServ.prototype.prepare = async function (user, hasData = false) {
-  const options = {
-    rejectUnauthorized: false,
-    followRedirects: this.follows,
-    secureProtocol: 'TLSv1_2_method',
-    headers: {},
-  };
-
-  if (user) {
-    let orgs = [];
-
-    if (Array.isArray(user.orgs)) {
-      orgs = user.orgs.map((o) => o.name.toLowerCase());
-    }
-
-    const data = {
-      teams: orgs,
-      orgs: orgs,
-      subscriber: user.subscriber,
-      id: user._id,
-      source: 'internal',
-    };
-
-    const jwt = await this.secrets.jwt(data, user._id);
-    if (jwt) {
-      options.headers.Authorization = `JWT-Bearer ${jwt}`;
-    }
-  }
-
-  if (hasData) {
-    options.headers['Content-Type'] = this.contentType;
-  }
-
-  return options;
+JobServ.prototype.find = async function ({ path, query, options }) {
+  return createResponse(this.get({ path, query, options }));
 };
 
 /**
- * Perform a find() search on the resource.
+ * Retrieve a resoruce by its id.
  *
- * @param {Object} user The user performing the action.
- * @param {Object} query Query/Search parameters.
- * @returns {Promise}
+ * @param {Object} data
+ * @param {String} data.id - The resource id.
+ * @param {Object} [data.query] - The query parameters.
+ * @param {Object} [data.options] - Optional request configurations.
+ * @returns {Promise<Object>}
  */
-JobServ.prototype.find = async function ({ user, path, query }) {
-  return createResponse(this.get(path, query, await this.prepare(user)));
-};
-
-/**
- * Perform a findById() search on the resource.
- *
- * @param {Object} user The user performing the action.
- * @param {String} id The ID of the resource to search.
- * @param {Object} query Query/Search parameters.
- * @returns {Promise}
- */
-JobServ.prototype.findById = async function ({ user, id, query }) {
-  return createResponse(this.get(id, query, await this.prepare(user)));
+JobServ.prototype.findById = async function ({ id, query, options }) {
+  return createResponse(this.get({ path: id, query, options }));
 };
 
 /**
  * Create a resource on the server.
  *
- * @param {Object} user The user performing the action.
- * @param {Object} data The data to send.
- * @param {String} path The path on the server where to perform the action.
- * @param {Object} query Query/Search parameters.
- * @returns {Promise}
+ * @param {Object} data
+ * @param {String} [data.path] - The path of the request.
+ * @param {String|Buffer} data.body - The data to send.
+ * @param {Object} [data.query] - The query parameters.
+ * @param {Object} [data.options] - Optional request configurations.
+ * @returns {Promise<Object>}
  */
-JobServ.prototype.create = async function ({ user, data, path, query }) {
-  return createResponse(
-    this.post(data, path, query, await this.prepare(user, true))
-  );
+JobServ.prototype.create = async function ({ path, body, query, options }) {
+  return createResponse(this.post({ path, body, query, options }));
 };
 
 /**
  * Update a resource on the server.
  *
- * @param {Object} user The user performing the action.
- * @param {Object} data The data to send.
- * @param {String} path The path on the server where to perform the action.
- * @param {Object} query Query/Search parameters.
- * @returns {Promise}
+ * @param {Object} data
+ * @param {String} [data.path] - The path of the request.
+ * @param {String|Buffer} data.body - The data to send.
+ * @param {Object} [data.query] - The query parameters.
+ * @param {Object} [data.options] - Optional request configurations.
+ * @returns {Promise<Object>}
  */
-JobServ.prototype.update = async function ({ user, data, path, query }) {
-  return createResponse(
-    this.patch(data, path, query, await this.prepare(user, true))
-  );
+JobServ.prototype.update = async function ({ path, body, query, options }) {
+  return createResponse(this.patch({ path, body, query, options }));
 };
 
 /**
  * Remove a resource on the server.
  *
- * @param {Object} user The user performing the action.
- * @param {String} path The path on the server where to perform the action.
- * @param {Object} query Query/Search parameters.
- * @returns {Promise}
+ * @param {Object} data
+ * @param {String} data.path - The path of the request.
+ * @param {Object} [data.query] - The query parameters.
+ * @param {Object} [data.options] - Optional request configurations.
+ * @returns {Promise<Object>}
  */
-JobServ.prototype.remove = async function ({ user, path, query }) {
-  return createResponse(this.delete(path, query, await this.prepare(user)));
+JobServ.prototype.remove = async function ({ path, query, options }) {
+  return createResponse(this.delete({ path, query, options }));
 };
 
 export default JobServ;
