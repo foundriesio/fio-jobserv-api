ef694044c3b26eaf8a963e232e388377aa567f2f
Milo Casagrande <milo@foundries.io>
2020-11-04 16:45:55 +0100

    update: factories resource
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/factories.js
+++ src/factories.js
@@ -6,23 +6,25 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 */
 
-import JobServ from './jobserv';
+import JobServ from './jobserv.js';
 
 class Factories extends JobServ {
-  constructor(uri, cache) {
-    super(uri, cache);
+  constructor(address) {
+    super(address);
+    this.basePath = '/ota/factories/';
   }
 }
 
 /**
  * Retrieve a factory status.
  * @param {Object} data
- * @param {Object} data.user
- * @param {string} data.factoryName
- * @param {Object} data.query
+ * @param {String} data.factory - The name of the factory.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
  */
-Factories.prototype.status = async function ({ user, factoryName, query }) {
-  return this.findById({ user, id: `${factoryName}/status/`, query });
+Factories.prototype.status = async function ({ factory, query, options }) {
+  return this.find({ path: `${factory}/status/`, query, options });
 };
 
 export default Factories;
