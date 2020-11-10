117de9aa8aacec88d2b644cb549133cab2e0408f
Milo Casagrande <milo@foundries.io>
2020-11-10 18:24:13 +0100

    feat: add Targets to factories
      * Add sub-class into the Factories one to retrieve
        targets.
    
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/factories.js
+++ src/factories.js
@@ -8,10 +8,67 @@ You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2
 
 import JobServ from './jobserv.js';
 
+class Targets extends JobServ {
+  constructor(address) {
+    super(address);
+    this.basePath = '/ota/factories/';
+  }
+}
+
+/**
+ * Create the correct target name to be retrieved.
+ * Right now it's `[target]-lmp-[build]`.
+ * @param {Object} data
+ * @param {Object} data.target - The target name.
+ * @param {Object} data.build - The build name/id.
+ * @returns {String}
+ */
+Targets.prototype.createTargetName = function ({ target, build }) {
+  return `${target}-lmp-${build}`;
+};
+
+/**
+ * List all targets of a factory.
+ * @param {Object} data
+ * @param {String} data.factory - The name of the factory.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
+ */
+Targets.prototype.list = async function ({ factory, query, options }) {
+  return this.find({ path: `${factory}/targets/`, query, options });
+};
+
+/**
+ * Get a target of a factory.
+ * @param {Object} data
+ * @param {String} data.factory - The name of the factory.
+ * @param {String} data.target - The name of the target.
+ * @param {String} data.build - The name/id of the build.
+ * @param {Object} [data.query] - The request query parameters.
+ * @param {Object} [data.options] - Optional request options.
+ * @returns {Promise<Object>}
+ */
+Targets.prototype.retrieve = async function ({
+  factory,
+  target,
+  build,
+  query,
+  options,
+}) {
+  return this.find({
+    path: `${factory}/targets/${this.createTargetName({ target, build })}/`,
+    query,
+    options,
+  });
+};
+
 class Factories extends JobServ {
   constructor(address) {
     super(address);
     this.basePath = '/ota/factories/';
+
+    this.Targets = new Targets(address);
   }
 }
 
